90fabbc54b622fe5662940ef787845914439169d
totallynotmanas <108781322+totallynotmanas@users.noreply.github.com>
2025-09-14 21:41:51 +0530

    skeleton code for mdel cachinng

---
+++ code/model_cache.py
@@ -0,0 +1,74 @@
+class EdgeModelCache:
+    def __init__(self, storage_limit):
+        pass
+
+    def add_model(self, model_id, model_size):
+        pass
+
+    def evict_model(self):
+        pass
+
+    def is_cached(self, model_id):
+        pass
+
+    def get_model(self, model_id):
+        pass
+
+    def update_cache_policy(self, model_id):
+        pass
+
+    def get_cache_status(self):
+        pass
+
+    def get_cache_hit_rate(self):
+        pass
+
+# Simulation / environment functions
+def handle_request(user_id, model_id):
+    pass
+
+def fetch_from_cloud(model_id):
+    pass
+
+def allocate_resources(model_id):
+    pass
+
+def run_simulation(requests):
+    pass
+class EdgeModelCache:
+    def __init__(self, storage_limit):
+        pass
+
+    def add_model(self, model_id, model_size):
+        pass
+
+    def evict_model(self):
+        pass
+
+    def is_cached(self, model_id):
+        pass
+
+    def get_model(self, model_id):
+        pass
+
+    def update_cache_policy(self, model_id):
+        pass
+
+    def get_cache_status(self):
+        pass
+
+    def get_cache_hit_rate(self):
+        pass
+
+# Simulation / environment functions
+def handle_request(user_id, model_id):
+    pass
+
+def fetch_from_cloud(model_id):
+    pass
+
+def allocate_resources(model_id):
+    pass
+
+def run_simulation(requests):
+    pass
