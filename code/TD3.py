8524c540e16b00f10bd02d641307d0abdeb4e684
Jai <jaisubiksha2006@gmail.com>
2025-09-15 13:38:52 +0530

    added actor and critic networks

--- code/TD3.py
+++ code/TD3.py
@@ -29,3 +29,51 @@ class ReplayBuffer:
             np.array(reward).reshape(-1, 1),
             np.array(done).reshape(-1, 1),
         )
+
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.layer1 = nn.Linear(state_dim, 400)
+        self.layer2 = nn.Linear(400, 300)
+        self.layer3 = nn.Linear(300, action_dim)
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.layer1(state))
+        a = F.relu(self.layer2(a))
+        return self.max_action * torch.tanh(self.layer3(a))
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        # Q1
+        self.l1 = nn.Linear(state_dim + action_dim, 400)
+        self.l2 = nn.Linear(400, 300)
+        self.l3 = nn.Linear(300, 1)
+        # Q2
+        self.l4 = nn.Linear(state_dim + action_dim, 400)
+        self.l5 = nn.Linear(400, 300)
+        self.l6 = nn.Linear(300, 1)
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+
+        q2 = F.relu(self.l4(sa))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2
+
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        return q1
+
